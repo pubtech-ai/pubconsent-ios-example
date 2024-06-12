006dd0d900f42a7f76dcaaa605ba8c769d1c7e60
Marco Prontera <prontera.marco.pm@gmail.com>
2024-06-12 11:02:04 +0200

    Initial Commit

---
+++ pubconsent-ios-example/pubconsent_ios_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  pubconsent_ios_exampleApp.swift
+//  pubconsent-ios-example
+//
+//  Created by Marco Prontera on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct pubconsent_ios_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
